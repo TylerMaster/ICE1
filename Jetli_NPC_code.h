abb76bbeeb34c083035cfab3d86f006e0c18fe97
TylerMaster <123766640+TylerMaster@users.noreply.github.com>
2026-01-24 15:27:02 -0500

    Create Jetli_NPC_code.h

---
+++ Jetli_NPC_code.h
@@ -0,0 +1,14 @@
+#ifndef JETLI_THESPECIALISTS_H
+#define JETLI_THESPECIALISTS_H
+
+
+#define JETLI_KICK  ( 1017 )
+#define JETLI_PUNCH  ( 1019 )
+#define JETLI_RUN ( 1009 )
+#define JETLI_JUMP  ( 1013 )
+#define JETLI_FLIP ( 1021 )
+
+#endif
+
+
+
